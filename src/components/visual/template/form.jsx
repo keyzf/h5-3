905c29fc2835b9386e776d5f9da0c420656889d6
antoninsorrento <antonin.chenying@gmail.com>
2018-05-30 17:35:52 +0800

    form
    form

---
+++ src/components/visual/template/form.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Row, Col, Icon } from 'antd';
+
+// 样式
+const default_text = {
+  alignItems: 'center',
+  border: '1px solid #ddd',
+  display: 'flex',
+  height: '50px',
+  justifyContent: 'center',
+  margin: 'auto',
+  width: '50px',
+};
+const icon_center = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: 'auto',
+};
+
+// 默认模板
+export const FormTemplate = () => {
+  return (
+    <Row type={'flex'} gutter={16}>
+      <Col span={6} style={default_text}>
+        <img src={''} alt={'img'} />
+      </Col>
+      <Col span={16}>
+        表单<br />
+        用于搜集信息
+      </Col>
+      <Col span={2} style={icon_center}>
+        <Icon type="plus-circle-o" style={{ fontSize: '18px' }} />
+      </Col>
+    </Row>
+  );
+};
