ff5dc1d56be1067e0eb86eeca22e62b355a76d82
antoninsorrento <antonin.chenying@gmail.com>
2018-09-14 15:49:14 +0800

    develop/1
    1.清空文件目录

--- src/redux/action.jsx
+++
@@ -1,9 +0,0 @@
-import { fromJS } from 'immutable';
-
-export const redux_action = (name, data, meta = false) => {
-  return {
-    type: name,
-    payload: fromJS(data),
-    meta: fromJS(meta),
-  };
-};
