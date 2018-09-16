74184ded8eabf8aee895b48e7be24982873491a0
antoninsorrento <antonin.chenying@gmail.com>
2018-09-16 10:01:47 +0800

    develop/2
    配置文pracel环境，但存在js文件打包过大的问题

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Button from 'antd/lib/button';
+
+class App extends React.PureComponent {
+    render() {
+        // const Item = Breadcrumb.Item;
+        return (
+            <div>
+                <Button type="primary">Button</Button>
+            </div>
+        )
+    }
+}
+
+export default App;
