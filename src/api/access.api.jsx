7665fa2530060b0b53989a8df274ec540f153d82
antoninsorrento <antonin.chenying@gmail.com>
2018-09-27 13:43:46 +0800

    修正渲染bug
    修正渲染bug

--- src/api/access.api.jsx
+++ src/api/access.api.jsx
@@ -39,20 +39,95 @@ export const access_api = (sid, state, up_func) => {
                 up_func("BG_UI", JSON.parse(response.data.info.bg));
               }
 
-              if (
-                response.data.info.ui !== "" &&
-                response.data.info.ui !== "[]"
-              ) {
-                up_func("H5_DATA", JSON.parse(response.data.info.ui));
+              if(response.data.info.ui === "[]"){
                 up_func(
                   "EDITOR_UI",
                   {
-                    data: JSON.parse(response.data.info.ui)[0],
+                    data: fromJS({
+                      customize: {
+                        type: "text",
+                        index_number: 0.3493262826073513,
+                        name: "horizontal_text",
+                        html_content:
+                          "<h2 style=\"text-align:center;\">横排文本示例</h2><br/><h4 style=\"text-align:center;font-size: 16px\">教程</h4><br/><p style=\"text-align:center;\">点左侧组件可以添加新内容</p><br/><p style=\"text-align:center;\">点一下可以进行删除等操作</p>"
+                      },
+                      advance: {
+                        color: "",
+                        img: "",
+                        width: 300,
+                        left: 12,
+                        move: true,
+                        height: 200,
+                        live: false,
+                        crop_img: "",
+                        top: 32,
+                        style_color: "",
+                        zIndex: 100,
+                        rotate: 0,
+                        img_config: {
+                          stretching: {
+                            value: ""
+                          },
+                          tiling: {
+                            value: ""
+                          }
+                        }
+                      }
+                    }),
                     number: 0,
                     com: true
                   },
                   { content: true, choose: true }
                 );
+
+              }else{
+                if (response.data.info.ui !== "") {
+                  up_func("H5_DATA", JSON.parse(response.data.info.ui));
+                  up_func(
+                    "EDITOR_UI",
+                    {
+                      data: JSON.parse(response.data.info.ui)[0],
+                      number: 0,
+                      com: true
+                    },
+                    { content: true, choose: true }
+                  );
+                }
+                else{
+                  up_func("H5_DATA", fromJS([
+                    {
+                      customize: {
+                        type: "text",
+                        index_number: 0.3493262826073513,
+                        name: "horizontal_text",
+                        html_content:
+                          "<h2 style=\"text-align:center;\">横排文本示例</h2><br/><h4 style=\"text-align:center;font-size: 16px\">教程</h4><br/><p style=\"text-align:center;\">点左侧组件可以添加新内容</p><br/><p style=\"text-align:center;\">点一下可以进行删除等操作</p>"
+                      },
+                      advance: {
+                        color: "",
+                        img: "",
+                        width: 300,
+                        left: 12,
+                        move: true,
+                        height: 200,
+                        live: false,
+                        crop_img: "",
+                        top: 32,
+                        style_color: "",
+                        zIndex: 100,
+                        rotate: 0,
+                        img_config: {
+                          stretching: {
+                            value: ""
+                          },
+                          tiling: {
+                            value: ""
+                          }
+                        }
+                      }
+                    }
+                  ]));
+                }
               }
             }
           });
@@ -86,20 +161,95 @@ export const access_api = (sid, state, up_func) => {
               if (response.data.info.bg) {
                 up_func("BG_UI", JSON.parse(response.data.info.bg));
               }
-              if (
-                response.data.info.ui !== "" &&
-                response.data.info.ui !== "[]"
-              ) {
-                up_func("H5_DATA", JSON.parse(response.data.info.ui));
+              if(response.data.info.ui === "[]"){
                 up_func(
                   "EDITOR_UI",
                   {
-                    data: JSON.parse(response.data.info.ui)[0],
+                    data: fromJS({
+                        customize: {
+                          type: "text",
+                          index_number: 0.3493262826073513,
+                          name: "horizontal_text",
+                          html_content:
+                            "<h2 style=\"text-align:center;\">横排文本示例</h2><br/><h4 style=\"text-align:center;font-size: 16px\">教程</h4><br/><p style=\"text-align:center;\">点左侧组件可以添加新内容</p><br/><p style=\"text-align:center;\">点一下可以进行删除等操作</p>"
+                        },
+                        advance: {
+                          color: "",
+                          img: "",
+                          width: 300,
+                          left: 12,
+                          move: true,
+                          height: 200,
+                          live: false,
+                          crop_img: "",
+                          top: 32,
+                          style_color: "",
+                          zIndex: 100,
+                          rotate: 0,
+                          img_config: {
+                            stretching: {
+                              value: ""
+                            },
+                            tiling: {
+                              value: ""
+                            }
+                          }
+                        }
+                      }),
                     number: 0,
                     com: true
                   },
                   { content: true, choose: true }
                 );
+
+              }else{
+                if (response.data.info.ui !== "") {
+                  up_func("H5_DATA", JSON.parse(response.data.info.ui));
+                  up_func(
+                    "EDITOR_UI",
+                    {
+                      data: JSON.parse(response.data.info.ui)[0],
+                      number: 0,
+                      com: true
+                    },
+                    { content: true, choose: true }
+                  );
+                }
+                else{
+                  up_func("H5_DATA", fromJS([
+                    {
+                      customize: {
+                        type: "text",
+                        index_number: 0.3493262826073513,
+                        name: "horizontal_text",
+                        html_content:
+                          "<h2 style=\"text-align:center;\">横排文本示例</h2><br/><h4 style=\"text-align:center;font-size: 16px\">教程</h4><br/><p style=\"text-align:center;\">点左侧组件可以添加新内容</p><br/><p style=\"text-align:center;\">点一下可以进行删除等操作</p>"
+                      },
+                      advance: {
+                        color: "",
+                        img: "",
+                        width: 300,
+                        left: 12,
+                        move: true,
+                        height: 200,
+                        live: false,
+                        crop_img: "",
+                        top: 32,
+                        style_color: "",
+                        zIndex: 100,
+                        rotate: 0,
+                        img_config: {
+                          stretching: {
+                            value: ""
+                          },
+                          tiling: {
+                            value: ""
+                          }
+                        }
+                      }
+                    }
+                  ]));
+                }
               }
             }
           });
